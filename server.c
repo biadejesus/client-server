8c25449334ea50a3c404f70134d257bf3ed0d56e
Bruna <bruna99stefany@gmail.com>
2019-10-21 12:52:43 -0300

    fazendo o handler

--- server.c
+++ server.c
@@ -3,10 +3,11 @@
 #include <stdlib.h>
 #include <sys/socket.h> // biblioteca que importa as funções do socket
 #include<arpa/inet.h> //sockaddr_in
+#include <pthread.h> // para utilizar o pthread para múltiplas conexões
 
 int main(){
     //criando um socket
-    int socket_descritor, new_socket , c;
+    int socket_descritor, new_socket , c, *new_sock;
     struct sockaddr_in server, client; // utiliza para conectar a um servidor remoto em um determinado número de porta. Para fazer isso é preciso de uma porta e um endereço de IP.
 
     // a função socket() cria uma socket e retorna um descritor que pode ser usado em outras funções.
@@ -30,5 +31,33 @@ int main(){
 
     //Listen
 	listen(socket_descritor , 3); // colocar os sockets em listening mode
+
+    //Aceita e conexões de entrada
+	puts("Esperando por conexões de entrada\n");
+	c = sizeof(struct sockaddr_in);
+	while( (new_socket = accept(socket_descritor, (struct sockaddr *)client, (socklen_t*)c)) )
+	{
+		puts("Conexão aceita\n");
+		
+		//Resposta para o cliente		
+		pthread_t sniffer_thread; // faz o threading
+		new_sock = malloc(1);
+		*new_sock = new_socket;
+		
+		if( pthread_create( sniffer_thread , NULL ,  connection_handler , (void*) new_sock) &lt; 0){
+			perror(&quot;could not create thread&quot;);
+			return 1;
+		}
+		
+		//Now join the thread , so that we dont terminate before the thread
+		//pthread_join( sniffer_thread , NULL);
+		puts(&quot;Handler assigned&quot;);
+
+        if (new_socket&lt;0)
+	{
+		perror(&quot;accept failed&quot;);
+		return 1;
+	}
+	}
 
 }
