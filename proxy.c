2999962064e0b6f9e3fe628d8c5ba8b6c8ec7531
biadejesus <ra104361@uem.br>
2019-10-24 12:37:50 -0300

    trocado para miaudota

---
+++ proxy.c
@@ -0,0 +1,3 @@
+//fazer bind pra descobrir a porta pra mandar pro server e no server recebendo do proxy
+//bind pra saber qual porta vai
+//bind do proxy com cliente e bind de cada server com o proxy
