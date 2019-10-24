5ae44a71b286482264f15c870369eb297e00eb15
biadejesus <ra104361@uem.br>
2019-10-24 17:24:00 -0300

    send, write e read

--- proxy.c
+++ proxy.c
@@ -1,3 +1,12 @@
 //fazer bind pra descobrir a porta pra mandar pro server e no server recebendo do proxy
 //bind pra saber qual porta vai
 //bind do proxy com cliente e bind de cada server com o proxy
+
+
+
+// if( recv(socket_desc, server_reply , 2000 , 0) < 0)
+// 	{
+// 		puts("recv failed");
+// 	}
+// 	puts("Reply received\n");
+// 	puts(server_reply);
