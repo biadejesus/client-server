91db320139035eb9e4704ae8874536ee00f41654
jmiguelzin <miguelarkjow@gmail.com>
2019-11-03 13:51:26 -0300

    socket in pipe

--- server-pipe.c
+++ server-pipe.c
@@ -1,29 +1,4 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/socket.h> // biblioteca que importa as funções do socket
-#include<arpa/inet.h> //sockaddr_in
-//#include <fcntl.h>
-
-#define post 1
-#define get 2
-#define TAM 100
-
-typedef struct{
-    char tipo[30];
-    int idade, ID;
-    char nome[30];
-}info;
-
-typedef struct{
-    info informacao;
-    int flag;
-	char resposta[30];
-}requisicao;
-
+#include "funcoes.c"
 
 void inicializarBD(info BD[]){
 	for(int i =0; i<TAM; i++){
@@ -37,29 +12,12 @@ int main(){
     int socket_desc, c,  client_sock, pipe_[2], socket_final;
 	requisicao requi;
 	info BD[TAM], animal;
-	inicializarBD(BD);
 
     struct sockaddr_in server, client; // utiliza para conectar a um servidor remoto em um determinado número de porta. Para fazer isso é preciso de uma porta e um endereço de IP.
 	pid_t process_id;
 
     // a função socket() cria uma socket e retorna um descritor que pode ser usado em outras funções.
-    socket_desc = socket(AF_INET, SOCK_STREAM, 0); //AF_INET( IP versãon 4) = tipo de endereços, SOCK_STREAM = tipo (isso significa protocolo TCP orientado a conexão), Protocolo - 0 [ ou IPPROTO_IP é o IP protocolo]
-    if (socket_desc == -1){
-        printf("Nao foi possivel criar o socket\n");
-    }
-    puts("Socket criado\n");
-
-    //conectando um socket ao server
-    server.sin_addr.s_addr = inet_addr("127.0.0.1"); // pega quaquer porta que está disponível no momento
-	server.sin_family = AF_INET;
-	server.sin_port = htons(8585); // porta aleatória
-
-    if (bind(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0){ // bind coloca o socket no lugar reservado para ele 
-		puts("Erro na conexão!");
-		return 1;
-	}
-	
-	puts("conectado\n");
+    socket_desc = criarSocket(8585);
 
     //Listen
 	listen(socket_desc , 3); // colocar os sockets em listening mode
@@ -85,17 +43,16 @@ int main(){
 			perror("Não foi possível estabelecer uma conexão!!");
 			return false;
 		}
-		if(process_id == 0){
+		else if(process_id == 0){
 			puts("a conexão foi estabelecida!");
 			read(pipe_[0], &BD, sizeof(BD));
-		
+		}
 			if(read(client_sock,&requi,sizeof(requi)) >= 0){
 				printf("\nENTROU IF");
-
 				switch (requi.flag){
 					case post:
 						printf("\nENTROU POST\n");
-						
+						inicializarBD(BD);
 						for(int i=0; i<TAM; i++){
 							if(BD[i].ID == -1){
 								printf("\nENTROU IF BD\n");
@@ -112,6 +69,7 @@ int main(){
 								break;
 							}
 						}
+						
 						puts("\nPost:\n");
 						printf("\tNome: %s\n", requi.informacao.nome);
 						printf("\tID: %d\n", requi.informacao.ID);
@@ -133,16 +91,14 @@ int main(){
 						//procurar no bd e retornar a struct com esse id e dar um write
 						break;
 					}
-					write(pipe_[1], &BD, sizeof(BD));
+
 					write(socket_final, &animal, sizeof(info));
-					
+					write(pipe_[1], &BD, sizeof(BD));
 				}
 			
 			else{
 				perror("Leitura da requisição falhou.\n");
-			} 
-			exit(1);
-		}
+			}
 		
 	}
 	if(socket_final < 0)
